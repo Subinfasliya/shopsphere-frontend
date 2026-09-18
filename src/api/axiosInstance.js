f2fce3ebd948089c60525d748aa83090fe2cf4e4
Subin <subinfasliya@gmail.com>
2026-09-18 16:12:29 +0530

    fix: remove default baseURL and log the current baseURL

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1';
+const baseURL = import.meta.env.VITE_API_URL;
+
+console.log(baseURL);
+
 
 const api = axios.create({
   baseURL,
